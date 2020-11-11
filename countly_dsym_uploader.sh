cf64900632c94c053224eab0d45686d510fb0784
yunpeng <yunpeng.kong@partner.bmw.com>
2020-11-11 14:26:01 +0800

    create tmp

---
+++ countly_dsym_uploader.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Creating archive of DSYM folder using zip
+
+DSYM_FILE_NAME="My BMW"
+DSYM_ZIP_PATH="/tmp/$(date +%s)_${DSYM_FILE_NAME}.txt"
+
+echo "Adding first line" > "$DSYM_ZIP_PATH"
+echo "Adding first line replaced" > "$DSYM_ZIP_PATH"
+echo "Appending second line " >> "$DSYM_ZIP_PATH"
+echo "Appending third line" >> "$DSYM_ZIP_PATH"
+
+echo "$DSYM_ZIP_PATH"
+
+cat "$DSYM_ZIP_PATH"
